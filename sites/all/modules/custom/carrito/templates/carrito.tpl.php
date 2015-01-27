dc56d607492ab2b299ca2ee500136572710ef7d9
Juan Carlos Orrego <jcorrego@gmail.com>
2015-01-27 15:23:37 -0500

    Styles adjustments in "Mi Carrito" block

--- sites/all/modules/custom/carrito/templates/carrito.tpl.php
+++ sites/all/modules/custom/carrito/templates/carrito.tpl.php
@@ -1,14 +1,18 @@
 <div class="carrito">
     <div class="row collapse">
-        <div class="columns small-12">Saldo disponible</div>
-        <div class="total columns small-12">$<?php echo number_format($data["saldo"],0,",",".")?></div>
-        <div class="columns small-12">Valor carrito</div>
-        <div class="total columns small-12">$<?php echo number_format($data["total"],0,",",".")?></div>
-        <div class="columns small-3 text-right">
-            <div class="basket right"></div>
+        <div class="data1">
+            <div class="columns small-12">Saldo disponible</div>
+            <div class="total columns small-12">$<?php echo number_format($data["saldo"],0,",",".")?></div>
         </div>
-        <div class="columns small-9">
-            <div class="count"><?php echo $data['count']?> <small>Jugador(es)</small></div>
+        <div class="data2">
+            <div class="columns small-12">Valor carrito</div>
+            <div class="total columns small-12">$<?php echo number_format($data["total"],0,",",".")?></div>
+            <div class="columns small-3 text-right">
+                <div class="basket right"></div>
+            </div>
+            <div class="columns small-9">
+                <div class="count"><?php echo $data['count']?> <small>Jugador(es)</small></div>
+            </div>
         </div>
     </div>
     <a class="checkout" href="/jugar/carrito">Ver Carrito</a>
