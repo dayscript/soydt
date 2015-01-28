e94a7380cf3918dca69065a6ca0c94f470ea543d
Juan Carlos Orrego <jcorrego@gmail.com>
2015-01-27 21:34:23 -0500

    Carrito de compra Styles

--- sites/all/modules/custom/carrito/templates/checkout.tpl.php
+++ sites/all/modules/custom/carrito/templates/checkout.tpl.php
@@ -1,16 +1,20 @@
 <div id="checkout">
-    <div class="subheader">Si esta seguro que desea comprar estos jugadores, haga click a continuación.</div>
-    <div class="clearfix">
-        <label class="left">Valor de la compra</label>
-        <div class="text-purple right">$<?php echo number_format($data["total"],0,",",".")?></div>
+    <div class="columns small-6 right">
+        <div class="subheader">Si esta seguro que desea comprar estos jugadores, haga click a continuación.</div>
     </div>
-    <div class="clearfix">
-        <label class="left">Saldo disponible</label>
-        <div class="text-purple right">$<?php echo number_format($data["saldo"],0,",",".")?></div>
+    <div class="columns small-6">
+        <div class="clearfix">
+            <label class="left">Valor de la compra</label>
+            <div class="text-purple right">$<?php echo number_format($data["total"],0,",",".")?></div>
+        </div>
+        <div class="clearfix">
+            <label class="left">Saldo disponible</label>
+            <div class="text-purple right">$<?php echo number_format($data["saldo"],0,",",".")?></div>
+        </div>
+        <?php if($data["total"]>0):?>
+            <a href="#" data-reveal-id="checkout-popup" data-reveal-ajax="/carrito/checkout" class="columns button small success">Comprar</a>
+        <?php endif?>
     </div>
-    <?php if($data["total"]>0):?>
-        <a href="#" data-reveal-id="checkout-popup" data-reveal-ajax="/carrito/checkout" class="columns button small success">Comprar</a>
-    <?php endif?>
 </div>
 <div id="checkout-popup" class="reveal-modal tiny" data-reveal data-options="close_on_background_click: false;" >
 </div>
