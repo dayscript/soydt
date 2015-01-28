295b1abadc58bc4159e8c4f901d227b4bcfdb478
Juan Carlos Orrego <jcorrego@gmail.com>
2015-01-27 21:35:36 -0500

    Carrito de compra Styles

--- sites/all/modules/custom/carrito/templates/checkout.tpl.php
+++ sites/all/modules/custom/carrito/templates/checkout.tpl.php
@@ -5,11 +5,11 @@
     <div class="columns small-6">
         <div class="clearfix">
             <label class="left">Valor de la compra</label>
-            <div class="text-purple right">$<?php echo number_format($data["total"],0,",",".")?></div>
+            <div class="text-blue right">$<?php echo number_format($data["total"],0,",",".")?></div>
         </div>
         <div class="clearfix">
             <label class="left">Saldo disponible</label>
-            <div class="text-purple right">$<?php echo number_format($data["saldo"],0,",",".")?></div>
+            <div class="text-blue right">$<?php echo number_format($data["saldo"],0,",",".")?></div>
         </div>
         <?php if($data["total"]>0):?>
             <a href="#" data-reveal-id="checkout-popup" data-reveal-ajax="/carrito/checkout" class="columns button small success">Comprar</a>
