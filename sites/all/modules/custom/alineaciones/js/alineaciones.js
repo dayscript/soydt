192ae1fb1ae038c6b7935933d0f5157a5bb58bb1
Juan Carlos Orrego <jcorrego@gmail.com>
2015-02-11 01:57:34 -0500

    Cambios en las alineaciones

--- sites/all/modules/custom/alineaciones/js/alineaciones.js
+++ sites/all/modules/custom/alineaciones/js/alineaciones.js
@@ -1,5 +1,5 @@
 jQuery( document ).ready(function() {
-    jQuery('#formacion').change( function(){
+    jQuery('.active #formacion').change( function(){
         jQuery('#alineaciones-popup').foundation('reveal', 'open', '/formacion/'+ jQuery("#id_alineacion").val()  +'/'+this.value);
     });
     jQuery('a.drop').click( function(){
@@ -8,14 +8,17 @@ jQuery( document ).ready(function() {
     jQuery('a.sell').click( function(){
         jQuery('#alineaciones-popup').foundation('reveal', 'open', '/carrito/sell'+'/'+this.id + '/' + jQuery("#id_alineacion").val());
     });
-    jQuery('#capitan').change( function(){
+    jQuery('.active #capitan').change( function(){
         jQuery('#alineaciones-popup').foundation('reveal', 'open', '/capitan/'+ jQuery("#id_alineacion").val()  +'/'+this.value);
+    });
+    jQuery('#fecha').change( function(){
+        window.location.href = "/jugar/alineaciones/" + this.value;
     });
     for(var i=1; i<=11; i++){
         var place = jQuery('#place'+i);
         var asigned = jQuery('#place'+i + '.asigned');
-        var position = jQuery('#place'+i+' .position').val();
-        place.droppable({accept:".player_position"+position, activeClass: "drop-hover",hoverClass:"drop-hover",tolerance: "intersect",
+        var position = jQuery('#cancha.active #place'+i+' .position').val();
+        place.droppable({accept:"#cancha.active .player_position"+position, activeClass: "drop-hover",hoverClass:"drop-hover",tolerance: "intersect",
             drop: function(event, ui) {
                 jQuery(this).animate({opacity:0});
                 jQuery('#alineaciones-popup').foundation('reveal', 'open', '/alinear/'+ jQuery("#id_alineacion").val() +'/' + this.id +'/'+ui.draggable[0].id);
@@ -32,6 +35,6 @@ jQuery( document ).ready(function() {
             place.animate({opacity:0});
         }
     }
-    jQuery('.player').draggable({snap:".place", snapMode: "inner",snapTolerance: 5,revert:'invalid'});
+    jQuery('#cancha.active .player').draggable({snap:".place", snapMode: "inner",snapTolerance: 5,revert:'invalid'});
 
 });
--- sites/all/modules/custom/alineaciones/templates/alineaciones.tpl.php
+++ sites/all/modules/custom/alineaciones/templates/alineaciones.tpl.php
@@ -1,20 +1,19 @@
-<?php $data = alineaciones_data()?>
-<div id="filtros" class="clearfix">
+<?php $data = alineaciones_data();?>
+<div id="filtros" class="clearfix <?php echo ($data["alineacion"]->field_fecha_torneo['und'][0]['target_id'] == $_SESSION['fecha_activa']->nid)?"active":""?>">
     <form>
         <div class="small-4 columns">
             <label>Fecha
                 <select name="fecha" id="fecha" class="text-center">
                     <?php foreach($data["fechas"] as $fecha):?>
-                        <?php if($fecha->field_fecha_abierta['und'][0]['value']==1):?>
-                            <option <?php echo ($fecha->nid==$data["alineacion"]->field_fecha_torneo['und'][0]['target_id'])?"selected":""?> class="text-center" value="<?php echo $fecha->nid?>"><?php echo $fecha->title?></option>
-                        <?php endif?>
+                        <option  <?php echo ($fecha->field_fecha_abierta['und'][0]['value']==1)?"":""?>  <?php echo ($fecha->nid==$data["alineacion"]->field_fecha_torneo['und'][0]['target_id'])?"selected":""?> class="text-center" value="<?php echo $fecha->nid?>"><?php echo $fecha->title?></option>
+                        <?php if($_SESSION['fecha_activa']->nid==$fecha->nid)break;?>
                     <?php endforeach?>
                 </select>
             </label>
         </div>
         <div class="small-4 columns">
             <label>Formaci&oacute;n
-                <select name="formacion" id="formacion" class="text-center">
+                <select name="formacion" id="formacion" class="text-center" <?php echo ($data["alineacion"]->field_fecha_torneo['und'][0]['target_id'] != $_SESSION['fecha_activa']->nid)?"disabled":""?>>
                     <?php foreach($data["formaciones"] as $formacion):?>
                         <?php if($formacion->tid==$data["alineacion"]->field_formacion['und'][0]['tid'])$form = $formacion;?>
                         <option <?php echo ($formacion->tid==$data["alineacion"]->field_formacion['und'][0]['tid'])?"selected":""?> class="text-center" value="<?php echo $formacion->tid?>"><?php echo $formacion->name?></option>
@@ -27,7 +26,7 @@
         </div>
         <div class="small-4 columns">
             <label>Capit&aacute;n
-                <select name="capitan" id="capitan" class="text-center">
+                <select name="capitan" id="capitan" class="text-center" <?php echo ($data["alineacion"]->field_fecha_torneo['und'][0]['target_id'] != $_SESSION['fecha_activa']->nid)?"disabled":""?>>
                     <option value="0">Seleccione un capit&aacute;n...</option>
                     <?php if(isset($data["equipo"]->field_jugadores2['und'])):?>
                         <?php foreach($data["equipo"]->field_jugadores2['und'] as $player):?>
@@ -42,7 +41,14 @@
         <input type="hidden" name="id_alineacion" id="id_alineacion" value="<?php echo $data["alineacion"]->nid?>">
     </form>
 </div>
-<div id="cancha" class="clearfix">
+<?php if ($data["alineacion"]->field_fecha_torneo['und'][0]['target_id'] == $_SESSION['fecha_activa']->nid):?>
+    <div data-alert class="alert-box info radius">
+        Nota: Recuerda que ya no es necesario guardar la alineación. Cada cambio que realices queda grabado automáticamente.
+        <a href="#" class="close">&times;</a>
+    </div>
+<?php endif?>
+<div></div>
+<div id="cancha" class="clearfix <?php echo ($data["alineacion"]->field_fecha_torneo['und'][0]['target_id'] == $_SESSION['fecha_activa']->nid)?"active":""?>">
     <?php for($i=1;$i<=11;$i++):?>
         <?php
         $var = "field_jugador".$i;
@@ -59,8 +65,9 @@
             <img src="/sites/all/themes/soydt_foundation/images/players/player<?php echo $positions[$i]["position"]?>.png">
         </div>
     <?php endfor?>
-    <?php if(isset($data["equipo"]->field_jugadores2['und'])):?>
-        <?php foreach($data["equipo"]->field_jugadores2['und'] as $player):?>
+    <?php for($i=1;$i<=11;$i++):?>
+        <?php if(isset($data["alineacion"]->{"field_jugador" . $i}['und']) && $data["alineacion"]->{"field_jugador" . $i}['und'][0]['target_id'] >0):?>
+            <?php $player = $data["alineacion"]->{"field_jugador" . $i}['und'][0]?>
             <?php $pl = node_load($player['target_id']);?>
             <div id="<?php echo $pl->nid?>" class="player player<?php echo $pl->nid?> player_position<?php echo $pl->field_posicion['und'][0]['tid']?> end columns text-center" >
                 <div class="image small-12 columns"><img src="/sites/all/themes/soydt_foundation/images/players/<?php echo $pl->field_equipo['und'][0]['target_id']?>.png"></div>
@@ -73,17 +80,38 @@
                 </div>
                 <div class="options columns small-10 small-centered text-center">
                     <a href="/node/<?php echo $pl->nid?>" class="info"></a>
-                    <a id="drop<?php echo $pl->nid?>" class="drop"></a>
-                    <a class="sell" id="sell<?php echo $pl->nid?>"></a>
+                    <?php if($data["alineacion"]->field_fecha_torneo['und'][0]['target_id'] == $_SESSION['fecha_activa']->nid):?>
+                        <a id="drop<?php echo $pl->nid?>" class="drop"></a>
+                        <a class="sell" id="sell<?php echo $pl->nid?>"></a>
+                    <?php endif?>
                 </div>
             </div>
-        <?php endforeach?>
-    <?php endif?>
+        <?php endif?>
+    <?php endfor ?>
     <div id="suplentes">
-        <?php if(isset($data["alineacion"]->field_suplentes['und'])):?>
-            <?php foreach($data["alineacion"]->field_suplentes['und'] as $player):?>
-                <img src="/sites/all/themes/soydt_foundation/images/players/player.png">
-            <?php endforeach?>
+        <?php if($data["alineacion"]->field_fecha_torneo['und'][0]['target_id'] != $_SESSION['fecha_activa']->nid || true):?>
+            <?php if(isset($data["alineacion"]->field_suplentes['und'])):?>
+                <?php foreach($data["alineacion"]->field_suplentes['und'] as $player):?>
+                    <?php $pl = node_load($player['target_id']);?>
+                    <div id="<?php echo $pl->nid?>" class="player player<?php echo $pl->nid?> player_position<?php echo $pl->field_posicion['und'][0]['tid']?> end columns text-center" >
+                        <div class="image small-12 columns"><img src="/sites/all/themes/soydt_foundation/images/players/<?php echo $pl->field_equipo['und'][0]['target_id']?>.png"></div>
+                        <div class="name_points columns small-12 text-center">
+                            <div class="columns small-9 name"><a href="/node/<?php echo $pl->nid?>"><?php echo explode(" ",$pl->field_apellidos['und'][0]['value'])[0]?></a></div>
+                            <div class="columns small-3 text-center points">0</div>
+                            <?php if(isset($data["alineacion"]->field_capitan['und']) && $pl->nid == $data["alineacion"]->field_capitan['und'][0]['target_id']):?>
+                                <div class="star"></div>
+                            <?php endif?>
+                        </div>
+                        <div class="options columns small-10 small-centered text-center">
+                            <a href="/node/<?php echo $pl->nid?>" class="info"></a>
+                            <?php if($data["alineacion"]->field_fecha_torneo['und'][0]['target_id'] == $_SESSION['fecha_activa']->nid):?>
+                                <a id="drop<?php echo $pl->nid?>" class="drop"></a>
+                                <a class="sell" id="sell<?php echo $pl->nid?>"></a>
+                            <?php endif?>
+                        </div>
+                    </div>
+                <?php endforeach?>
+            <?php endif?>
         <?php endif?>
     </div>
 </div>
