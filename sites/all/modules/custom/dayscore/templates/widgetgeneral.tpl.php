1f13facb248d4993e6cc9546739f9e10cef5ff0a
Juan Carlos Orrego <jcorrego@gmail.com>
2015-02-12 17:04:26 -0500

    Modulo de widget FPC

---
+++ sites/all/modules/custom/dayscore/templates/widgetgeneral.tpl.php
@@ -0,0 +1,3 @@
+<?php
+//print_r($data);?>
+<iframe id="widget-ds-iframe-aguila" style="border:0;width:100%;height:600px" scrolling="no" frameborder="0" height="600" width="100%" allowtransparency="true" src="//widgets.dayscript.com/clients/v01/?height=600&amp;id=widget-ds-iframe-aguila&amp;theme=dimayor&amp;width=&amp;rand=<?php echo rand(1,10000)?>"></iframe>
