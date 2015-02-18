cc8ecde37c9bbbd8c6bfa198f4148dbcbb946ebd
Juan Carlos Orrego <jcorrego@gmail.com>
2015-02-18 17:01:14 -0500

    Plantilla de bloque de carrito responsive

--- sites/all/modules/custom/carrito/templates/carrito.tpl.php
+++ sites/all/modules/custom/carrito/templates/carrito.tpl.php
@@ -1,10 +1,10 @@
 <div class="carrito">
     <div class="row collapse">
-        <div class="data1">
+        <div class="data1 columns small-12 medium-6">
             <div class="columns small-12">Saldo disponible</div>
             <div class="total columns small-12">$<?php echo number_format($data["saldo"],0,",",".")?></div>
         </div>
-        <div class="data2">
+        <div class="data2 columns small-12 medium-6">
             <div class="columns small-12">Valor carrito</div>
             <div class="total columns small-12">$<?php echo number_format($data["total"],0,",",".")?></div>
             <div class="columns small-3 text-right">
