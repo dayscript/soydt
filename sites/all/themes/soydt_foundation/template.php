ab6ec67546585adfcbb00052248cd8ca80344f2f
Juan Carlos Orrego <jcorrego@gmail.com>
2015-03-17 01:24:19 -0500

    Style updates, changing grid columns to 16

--- sites/all/themes/soydt_foundation/template.php
+++ sites/all/themes/soydt_foundation/template.php
@@ -16,8 +16,37 @@
  * Implements template_preprocess_page
  *
  */
-//function soydt_foundation_preprocess_page(&$variables) {
-//}
+function soydt_foundation_preprocess_page(&$variables) {
+    // Convenience variables.
+    if (!empty($variables['page']['sidebar_first'])) {
+        $left = $variables['page']['sidebar_first'];
+    }
+
+    if (!empty($variables['page']['sidebar_second'])) {
+        $right = $variables['page']['sidebar_second'];
+    }
+    // Dynamic sidebars.
+    if (!empty($left) && !empty($right)) {
+        $variables['main_grid'] = 'medium-6 medium-push-5';
+        $variables['sidebar_first_grid'] = 'medium-5 medium-pull-6';
+        $variables['sidebar_sec_grid'] = 'medium-5';
+    }
+    elseif (empty($left) && !empty($right)) {
+        $variables['main_grid'] = 'medium-11';
+        $variables['sidebar_first_grid'] = '';
+        $variables['sidebar_sec_grid'] = 'medium-5';
+    }
+    elseif (!empty($left) && empty($right)) {
+        $variables['main_grid'] = 'medium-11 medium-push-5';
+        $variables['sidebar_first_grid'] = 'medium-5 medium-pull-11';
+        $variables['sidebar_sec_grid'] = '';
+    }
+    else {
+        $variables['main_grid'] = '';
+        $variables['sidebar_first_grid'] = '';
+        $variables['sidebar_sec_grid'] = '';
+    }
+}
 
 /**
  * Implements template_preprocess_node
