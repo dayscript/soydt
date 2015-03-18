69d79267474fa2ec4fd0e8a5f9a372874839ef69
Jorge M <dayscript@Mac-Pro-de-Dayscript.local>
2015-03-18 18:24:42 -0500

    Fixing Theme Structure

--- sites/all/themes/soydt_foundation/Gruntfile.js
+++ sites/all/themes/soydt_foundation/Gruntfile.js
@@ -1,13 +1,49 @@
 module.exports = function(grunt) {
-  "use strict";
+  require('load-grunt-tasks')(grunt);
 
   var theme_name = 'soydt_foundation';
+  var base_theme_path = '../zurb_foundation';
 
   var global_vars = {
+    base_theme_path: base_theme_path,
     theme_name: theme_name,
     theme_css: 'css',
     theme_scss: 'scss'
-  }
+  };
+
+  var bourbon = require('node-bourbon').includePaths;
+
+  // array of javascript libraries to include.
+  var jsLibs = [
+    '<%= global_vars.base_theme_path %>/js/vendor/placeholder.js',
+    '<%= global_vars.base_theme_path %>/js/vendor/fastclick.js'
+  ];
+
+  // array of foundation javascript components to include.
+  var jsFoundation = [
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.abide.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.accordion.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.alert.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.clearing.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.dropdown.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.equalizer.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.interchange.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.joyride.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.magellan.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.offcanvas.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.orbit.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.reveal.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.slider.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.tab.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.tooltip.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.topbar.js'
+  ];
+
+  // array of custom javascript files to include.
+  var jsApp = [
+    'js/_*.js'
+  ];
 
   grunt.initConfig({
     global_vars: global_vars,
@@ -16,9 +52,9 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         options: {
-          // outputStyle: 'compressed',
           outputStyle: 'compact',
-          includePaths: ['<%= global_vars.theme_scss %>', require('node-bourbon').includePaths],
+          // outputStyle: 'compressed',
+          includePaths: ['<%= global_vars.theme_scss %>', '<%= global_vars.base_theme_path %>/scss/'].concat(bourbon),
           sourceMap: true
         },
         files: {
@@ -27,20 +63,23 @@ module.exports = function(grunt) {
       }
     },
 
-    copy: {
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        jsApp
+      ]
+    },
+
+    uglify: {
       dist: {
-        files: [
-          {expand:true, cwd: 'bower_components/foundation/js', src: ['foundation/*.js'], dest: 'js/', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/foundation/', src: ['foundation.min.js'], dest: 'js/', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/foundation/js/vendor', src: ['fastclick.js'], dest: 'js/vendor', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/foundation/js/vendor', src: ['jquery.cookie.js'], dest: 'js/vendor', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/foundation/js/vendor', src: ['modernizr.js'], dest: 'js/vendor', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/foundation/scss/foundation/components', src: '**/*.scss', dest: 'scss/vendor/foundation/components', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/foundation/scss/foundation', src: '_functions.scss', dest: 'scss/vendor/foundation', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/foundation/scss/foundation', src: '_foundation.scss', dest: 'scss/vendor/foundation', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/foundation/scss/foundation', src: '_normalize.scss', dest: 'scss/vendor/foundation', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/foundation/scss/foundation', src: '_variables.scss', dest: 'scss/vendor/foundation', filter: 'isFile'},
-        ]
+        files: {
+          'js/dist/libs.min.js': [jsLibs],
+          'js/dist/foundation.min.js': [jsFoundation],
+          'js/dist/app.min.js': [jsApp]
+      }
       }
     },
 
@@ -53,14 +92,19 @@ module.exports = function(grunt) {
         options: {
           livereload: true
         }
+      },
+
+      js: {
+        files: [
+          jsLibs,
+          jsFoundation,
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint', 'uglify']
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('build', ['sass', 'copy']);
+  grunt.registerTask('build', ['jshint','uglify','sass']);
   grunt.registerTask('default', ['build', 'watch']);
-}
+};
