98875f4b39bd416933971732a09bc99d9f33db24
Juan Carlos Orrego <jcorrego@gmail.com>
2015-06-22 08:02:01 -0500

    Error corrected

--- sites/all/modules/custom/_soydt_core/inc/functions.php
+++ sites/all/modules/custom/_soydt_core/inc/functions.php
@@ -5,10 +5,13 @@
  */
   function existe_futbolista_en_referencia_entidad ($id_ftb, $ids_futbolistas) {
     $id_ftb = (int) $id_ftb;
-    foreach ($ids_futbolistas as $jugador) {
-      if ($jugador['target_id'] == $id_ftb)
-        return true;
-    }
+      if(is_array($ids_futbolistas )){
+          foreach ($ids_futbolistas as $jugador) {
+              if ($jugador['target_id'] == $id_ftb)
+                  return true;
+          }
+
+      }
   }
 
 
