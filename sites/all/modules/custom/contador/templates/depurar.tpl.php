df7e161fc88fa5e5e2fd1292bc5eb838d3d1d09d
Juan Carlos Orrego <jcorrego@gmail.com>
2015-07-06 12:47:24 -0500

    Depurar

--- sites/all/modules/custom/contador/templates/depurar.tpl.php
+++ sites/all/modules/custom/contador/templates/depurar.tpl.php
@@ -2,7 +2,7 @@
 foreach($data as $key=>$row){
     echo "<div style='padding:20px;border-bottom:1px solid black;'>";
     echo "<pre>";
-    print_r($key);
+    //print_r($key);
     print_r($row);
     echo "</pre>";
     echo "</div>";
