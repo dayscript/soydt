fc8cfe0cbb9eef68bc480e5a4287fba4083dac60
Juan Carlos Orrego <jcorrego@gmail.com>
2015-07-06 12:13:25 -0500

    Depurar

--- sites/all/modules/custom/contador/templates/depurar.tpl.php
+++ sites/all/modules/custom/contador/templates/depurar.tpl.php
@@ -1,7 +1,8 @@
 <?php
-foreach($data as $row){
+foreach($data as $key=>$row){
     echo "<div style='padding:20px;border-bottom:1px solid black;'>";
     echo "<pre>";
+    print_r($key);
     print_r($row);
     echo "</pre>";
     echo "</div>";
